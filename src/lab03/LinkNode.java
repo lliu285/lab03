4ee6be7311a9a529494aa8a1ce314945b5216837
Lucia Liu <112028165+lliu285@users.noreply.github.com>
2024-05-16 21:59:53 -0700

    worked on SinglyLinkedList

--- src/lab03/LinkNode.java
+++ src/lab03/LinkNode.java
@@ -9,10 +9,37 @@ package lab03;
 public class LinkNode 
 {
 	private Currency data; // pointer/reference??
-	private Currency next;
+	private LinkNode next;
 	
 	public LinkNode()
 	{
-		
+		 data = null;
+		 next = null;
+	}
+	
+	public LinkNode(Currency data, LinkNode next)
+	{
+		 this.data = data;
+		 this.next = next;
+	}
+	
+	public Currency getData()
+	{
+		return data;
+	}
+	
+	public void setData(Currency data)
+	{
+		this.data = data;
+	}
+	
+	public LinkNode getNext()
+	{
+		return next;
+	}
+	
+	public void setNext(LinkNode next)
+	{
+		this.next = next;
 	}
 }
--- src/lab03/SinglyLinkedList.java
+++ src/lab03/SinglyLinkedList.java
@@ -2,15 +2,15 @@ package lab03;
 
 public class SinglyLinkedList 
 {
-	private int count;
+	private int count; // is count the total number of nodes??
 	private LinkNode start;
 	private LinkNode end;
 	
 	public SinglyLinkedList()
 	{
 		count = 0;
-		start = new LinkNode();
-		end = new LinkNode();
+		start = null;
+		end = null;
 	}
 	
 	public int getCount()
@@ -38,40 +38,127 @@ public class SinglyLinkedList
 		
 	}
 	
+	/*
+	 * addCurrency method which takes a Currency object 
+	 * and a node index value as parameters to add the Currency to the list at that index.
+	 */
 	public void addCurrency(Currency curr, int i)
 	{
+		LinkNode newNode = new LinkNode(curr, null);
 		
+		if (start == null) { // Insert as list's first node
+			start = newNode;
+			end = newNode;
+		} else if (start != null && i == count) { // Insert after list's tail node
+			end.setNext(newNode);
+			end = newNode; 
+		} else { // Insert in middle of list
+			
+			// use getCurrency() method or write another method?? this code's kinda redundant
+			
+			LinkNode currNode = start;
+			
+			for (int j = 0; j < i; j++) {
+				currNode = currNode.getNext();
+			}
+			
+			newNode.setNext(currNode.getNext());
+			currNode.setNext(newNode);
+		}
+		
+		count++;
 	}
 	
-	public Currency removeCurrency(int i)
+	/*
+	 * removeCurrency method which takes a Currency object as parameter 
+	 * and removes that Currency object from the list and may return a copy of the Currency.
+	 */
+	public Currency removeCurrency(Currency curr)
 	{
-		Currency curr = new Dollar();
+		// any preconditions?? like if the list was empty or smth
+		
+		LinkNode prevNode = start;
+		LinkNode nextNode = start.getNext();
+		
+		while (!prevNode.getNext().getData().isEqual(curr)) {
+			prevNode = prevNode.getNext();
+			nextNode = nextNode.getNext();
+		}
+		
+		prevNode.setNext(nextNode.getNext());
+		count--;
+
 		return curr;
 	}
 	
+	/*
+	 * removeCurrency overload method which takes a node index as parameter 
+	 * and removes the Currency object at that index and may return a copy of the Currency
+	 */
+	public Currency removeCurrency(int i)
+	{
+		// any preconditions?? like if the list was empty or smth
+		
+		LinkNode prevNode = start;
+		LinkNode nextNode = start.getNext();
+		
+		for (int j = 0; j < i-1; j++) {
+			prevNode = prevNode.getNext();
+			nextNode = nextNode.getNext();
+		}
+
+		Currency curr = prevNode.getNext().getData();
+		prevNode.setNext(nextNode.getNext());
+		count--;
+
+		return curr;
+	}
+	
+	/*
+	 * findCurrency method which takes a Currency object as parameter 
+	 * and returns the node index at which the Currency is found in the list.
+	 */
 	public int findCurrency(Currency curr)
 	{
-		return 0;
+		int i = 0;
+		LinkNode currNode = start;
+		
+		while (!currNode.getData().isEqual(curr)) {
+			currNode.setNext(currNode.getNext());
+			i++;
+		}
+		
+		return i;
 	}
 	
 	public Currency getCurrency(int i)
 	{
-		Currency curr = new Dollar();
-		return curr;
+		LinkNode currNode = start;
+
+		for (int j = 0; j < i; j++) {
+			currNode = currNode.getNext();
+		}
+		
+		return currNode.getData();
 	}
 	
 	public String printList()
 	{
+		for (int i = 0; i < count; i++) {
+			
+		}
+		
+		
 		return "";
 	}
 	
 	public boolean isListEmpty()
 	{
-		return false;
+		return start == null;
 	}
 	
 	public int countCurrency()
 	{
-		return 0;
+		return count;
 	}
 }
